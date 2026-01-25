1697732406fedd8c9c89bfbeeab8d65a306c5f7b
Duong Vu Cong Tuan <DuongVCT.B22KH024@stu.ptit.edu.vn>
2026-01-25 14:33:03 +0700

    fix: remove default value at column 'deleted_at'

--- backend/api/sql/schema/1_2026_01_25_users.sql
+++ backend/api/sql/schema/1_2026_01_25_users.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     created_at TIMESTAMP NOT NULL DEFAULT now(),
     updated_at TIMESTAMP NOT NULL DEFAULT now(),
-    deleted_at TIMESTAMP NULL DEFAULT now(),
+    deleted_at TIMESTAMP NULL ,
     email TEXT NOT NULL UNIQUE,
     name VARCHAR(200) NOT NULL,
     password TEXT NOT NULL DEFAULT 'unset'
